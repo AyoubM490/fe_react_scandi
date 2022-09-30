4c7fbe592e8101045e9809a56899d92c1ed058d2
AyoubMS <chahayb563@gmail.com>
2022-09-30 19:36:20 +0100

    add addProduct component

---
+++ src/components/Forms/BookForm.component.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BookForm = ({ weight, setWeight }) => {
+  const onChangeWeight = (e) => {
+    setWeight(e.target.value);
+  };
+  return (
+    <div className="book-form" id="Book">
+      <div className="notice">! Please provide weight in KG format</div>
+      <div className="form-group">
+        <label className="label" htmlFor="book-weight">
+          Weight (KG)
+        </label>
+        <input
+          type="number"
+          value={weight}
+          onChange={onChangeWeight}
+          className="form-control"
+          id="book-weight"
+        />
+      </div>
+    </div>
+  );
+};
+
+BookForm.propTypes = {
+  weight: PropTypes.number.isRequired,
+  setWeight: PropTypes.func.isRequired,
+};
+
+export default BookForm;
